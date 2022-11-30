753834207f1a9237c7fc0b31ed355ac8dcb6fcf0
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-11-30 14:50:54 +0100

    feat: DTO for the subscriptions

--- functions/get-subscriptions.js
+++ functions/get-subscriptions.js
@@ -1,8 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { faunaFetch } = require('./utils/fauna');
+const {faunaFetch} = require('./utils/fauna');
 
 exports.handler = async (_event, context) => {
-    const { user } = context.clientContext;
+    const {user} = context.clientContext;
 
     const result = await faunaFetch({
         query: `
@@ -17,15 +17,37 @@ exports.handler = async (_event, context) => {
         },
     });
 
-    const { stripeID } = result.data.getUserByNetlifyID;
+    const {stripeID} = result.data.getUserByNetlifyID;
 
     // See https://stripe.com/docs/api/subscriptions/list?lang=node
     const subscriptions = await stripe.subscriptions.list({
         customer: stripeID
     });
 
+    // Cache for the products
+    const products = {};
+
+    // Getting only the minimal amount of data
+    const model = await Promise.all(subscriptions.data.map(async (subscription) => {
+        const priceName = subscription.plan.nickname;
+        const productId = subscription.plan.product;
+        let product = products[productId];
+        if (!product) {
+            product = await stripe.products.retrieve(productId);
+            products[productId] = product;
+        }
+        return {
+            id: subscription.id,
+            metadata: subscription.metadata,
+            price: priceName,
+            product: product.name
+        };
+    }));
+
+    console.log("model = ", model);
+
     return {
         statusCode: 200,
-        body: JSON.stringify(subscriptions.data),
+        body: JSON.stringify(model),
     };
 };
